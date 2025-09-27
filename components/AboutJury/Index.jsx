231e05525e0dc7efab5431c9188c6878303776ee
Amaan MK <syedkhalander66@gmail.com>
2025-09-27 14:19:16 +0530

    chore: updated highlights format.

--- components/AboutJury/Index.jsx
+++ components/AboutJury/Index.jsx
@@ -12,53 +12,61 @@ import ParagSection2 from "../helpers/ParagSection2";
 import SectionTitle from "../helpers/SectionTitle";
 
 const juryData = [
-    {
+  {
     title: "Nithin Kamath",
-    paragraph: "Nithin Kamath is a prominent figure in the technology sector, currently serving as the Executive Director and founder of Capulus Technologies Private Limited based in Karnataka, India. With over a decade of hands-on experience in the tech industry, Nithin has demonstrated a strong commitment to leadership and innovation. His journey at CapulusTech has been fueled by a passion for developing technology-driven solutions that redefine user experiences and address the evolving demands of the market. As a leader, Nithin emphasizes the importance of crafting technology solutions that have a tangible impact on daily life. Under his guidance, the team at Capulus prioritizes expertise in software development, ensuring that the products they deliver not only meet market needs but also contribute meaningfully to technological advancement. This dedication to innovation reflects Nithin's belief in leveraging technology to improve lives and create value.",
+    paragraph:
+      "Nithin Kamath is a prominent figure in the technology sector, currently serving as the Executive Director and founder of Capulus Technologies Private Limited based in Karnataka, India. With over a decade of hands-on experience in the tech industry, Nithin has demonstrated a strong commitment to leadership and innovation. His journey at CapulusTech has been fueled by a passion for developing technology-driven solutions that redefine user experiences and address the evolving demands of the market. As a leader, Nithin emphasizes the importance of crafting technology solutions that have a tangible impact on daily life. Under his guidance, the team at Capulus prioritizes expertise in software development, ensuring that the products they deliver not only meet market needs but also contribute meaningfully to technological advancement. This dedication to innovation reflects Nithin's belief in leveraging technology to improve lives and create value.",
     highlights: [
       "Executive Director",
-      "Founder of Capulus Technologies Private Limited",
-      "Over a decade of hands-on experience",
-      "Demonstrated a strong commitment to leadership and innovation",
-      "Technology-driven solutions",
-      "Tangible impact on daily life",
-      "Expertise in software development",
-      "Passion for developing technology-driven solutions"
+      "founder of Capulus Technologies Private Limited",
+      "over a decade of hands-on experience",
+      "strong commitment to leadership and innovation",
+      "passion for developing technology-driven solutions",
+      "technology solutions that have a tangible impact",
+      "expertise in software development"
     ],
     image: NithinKamath,
     orderLast: false,
   },
-  {
-    title: "Amaan Mohammad Khalander",
-    paragraph: "Amaan Mohammad Khalander is an alumnus of the AI & ML branch at AIT, Karnataka. With a strong foundation in full-stack development and expertise in artificial intelligence and machine learning, he has consistently demonstrated a passion for building innovative solutions. Recently, he founded Royal Software Pvt Ltd, a company dedicated to delivering advanced technology services, skill training programs, and custom solutions for startups, businesses, and institutions. Through his entrepreneurial journey, Amaan continues to bridge education and industry, empowering the next generation of tech professionals.",
-     highlights: [
-  "full-stack development",
-  "Founder of Royal Software Pvt Ltd",
-  "Strong knowledge in Full Stack Development & AI/ML",
-  "Focus on technology services & skill training programs",
-  "Entrepreneur driving innovation in the tech industry",
-  "artificial intelligence and machine learning",
-  "he founded Royal Software Pvt Ltd",
-  "a company dedicated to delivering advanced technology services, skill training programs, and custom solutions for startups, businesses, and institutions"
-],
-    image: aman,
-    orderLast: true,
-  },
+{
+  title: "Amaan Mohammad Khalander",
+  paragraph:
+    "Amaan Mohammad Khalander is an alumnus of the AI & ML branch at AIT, Karnataka. With strong full-stack and AI/ML expertise, he has a passion for building innovative solutions. He founded Automatech, a government-registered technology startup focused on tools and platforms simplifying software and machine learning development. Key projects include MULTIDE and the Mul-Model platform, a final year project facilitating collaboration on machine learning models. Mul-Model acts like GitHub for ML and data models, offering pretrained models for tasks like command processing, image recognition, and data analysis. Automatech’s Mul-Command provides interactive commands for training, testing, and deploying models for beginners and experts. The company builds machine learning pipelines to automate backend workflows and has been recognized by the Education Excellence Foundation.",
+  highlights: [
+    "alumnus of the AI & ML branch at AIT",
+    "strong full-stack and AI/ML expertise",
+    "founded Automatech",
+    "government-registered technology startup",
+    "tools and platforms simplifying software and machine learning development",
+    "MULTIDE and the Mul-Model platform",
+    "final year project",
+    "Mul-Model acts like GitHub for ML and data models",
+    "offering pretrained models for command processing, image recognition, and data analysis",
+    "Mul-Command provides interactive commands for model training, testing, and deployment",
+    "machine learning pipelines to automate backend workflows",
+    "recognized by the Education Excellence Foundation"
+  ],
+  image: aman,
+  orderLast: true
+},
   {
     title: "Rohith Krishna G R",
-    paragraph: "Rohith Krishna G R is a final-year ECE student at Shri Siddhartha Institute of Technology, Tumkur. Driven by his passion for entrepreneurship, he founded a community-based startup, WETEAMRK7, which is incubated at Shri Siddhartha Academy of Higher Education. Through this venture, he is serving multiple clients both in India and abroad.He has already partnered with Adichunchanagiri Institute of Technology, Chikkamagaluru, as the Internship & Learning Partner for Smart Hack - 2K25 (Smart India Internal Hackathon 2025), where he provided internship opportunities and free Udemy access to the top five teams from a pool of 49 teams. Alongside his entrepreneurial journey, he is also contributing to global research initiatives as a NASA Citizen Scientist, reflecting his commitment to both innovation and knowledge-sharing.",
+    paragraph:
+      "Rohith Krishna G R is a final-year ECE student at Shri Siddhartha Institute of Technology, Tumkur. Driven by his passion for entrepreneurship, he founded a community-based startup, WETEAMRK7, which is incubated at Shri Siddhartha Academy of Higher Education. Through this venture, he is serving multiple clients both in India and abroad.He has already partnered with Adichunchanagiri Institute of Technology, Chikkamagaluru, as the Internship & Learning Partner for Smart Hack - 2K25 (Smart India Internal Hackathon 2025), where he provided internship opportunities and free Udemy access to the top five teams from a pool of 49 teams. Alongside his entrepreneurial journey, he is also contributing to global research initiatives as a NASA Citizen Scientist, reflecting his commitment to both innovation and knowledge-sharing.",
     highlights: [
-  "Final-year ECE student at SSIT, Tumkur",
-  "Founder of WETEAMRK7 (incubated at SSAHE)",
-  "Serving multiple clients in India & abroad",
-  "Internship & Learning Partner at Smart Hack - 2K25, AIT Chikkamagaluru",
-  "Provided internships & Udemy access to top hackathon teams",
-  "NASA Citizen Scientist contributing to global research"
-],  
+      "final-year ECE student at Shri Siddhartha Institute of Technology, Tumkur",
+      "founded community-based startup, WETEAMRK7",
+      "incubated at Shri Siddhartha Academy of Higher Education",
+      "serving multiple clients both in India and abroad",
+      "partnered with Adichunchanagiri Institute of Technology, Chikkamagaluru",
+      "Internship & Learning Partner for Smart Hack - 2K25 (Smart India Internal Hackathon 2025)",
+      "provided internship opportunities and free Udemy access to the top five teams",
+      "contributing to global research initiatives as a NASA Citizen Scientist",
+      "commitment to both innovation and knowledge-sharing"
+    ],
     image: rohith,
     orderLast: false,
   }
-
 ];
 
 const AboutJury = () => {
@@ -85,7 +93,7 @@ const AboutJury = () => {
   }, []);
 
   // const sectionOpacity = useTransform(scrollY, scrollRanges, [1, 0.3]);
-const sectionOpacity = 1;
+  const sectionOpacity = 1;
   return (
     <section className="relative lg:mb-28" id="about-jury">
       <div className="relative">
@@ -104,10 +112,10 @@ const sectionOpacity = 1;
           className="section-container pt-6 cursor-pointer"
         >
           <SectionTitle title={"About Jury's"} />
-          
-          {juryData.length==0?(<div>
+
+          {juryData.length == 0 ? (<div>
             <p className="text-center mt-10 font-semibold text-xl text-white">Jury details will be updated soon. Stay tuned!</p>
-          </div>):juryData.map((jury, index) => (
+          </div>) : juryData.map((jury, index) => (
             <div key={index} className={`flex flex-col gap-10 items-center lg:flex-row mt-[3rem] lg:m-12 ${jury.orderLast ? "lg:flex-row-reverse" : ""}`}>
               <div className={`relative w-[250px] lg:w-[321px]`}>
                 <Tilt className="cursor-pointer">
@@ -118,7 +126,7 @@ const sectionOpacity = 1;
                 <ParagSection2
                   title={jury.title}
                   paragraph={jury.paragraph}
-                  highlights={jury.highlights} 
+                  highlights={jury.highlights}
                 />
               </div>
             </div>
